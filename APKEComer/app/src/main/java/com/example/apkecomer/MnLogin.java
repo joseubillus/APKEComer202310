d632dac87de07baecb676ea43a5d09c9852f33e2
joseubillus <jose_ubillus@hotmail.com>
2023-02-15 21:04:29 -0500

    prueba1

--- APKEComer/app/src/main/java/com/example/apkecomer/MnLogin.java
+++ APKEComer/app/src/main/java/com/example/apkecomer/MnLogin.java
@@ -2,7 +2,9 @@ package com.example.apkecomer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MnLogin extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MnLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mn_login);
+    }
+
+    public void Onclick_btnAcep(View v){
+        startActivity(new Intent(MnLogin.this,MnMenu.class));
     }
 }
