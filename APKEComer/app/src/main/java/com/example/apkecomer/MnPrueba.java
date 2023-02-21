0ed1a06c1fdbfaa9a2700f324f45fecc71854641
joseubillus <jose_ubillus@hotmail.com>
2023-02-20 19:14:21 -0500

    prueba1

--- APKEComer/app/src/main/java/com/example/apkecomer/MnPrueba.java
+++ APKEComer/app/src/main/java/com/example/apkecomer/MnPrueba.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.SearchView;
 
 import com.example.controlador.DPrueba1;
 
 public class MnPrueba extends AppCompatActivity {
+    private SearchView schbus;
     private ListView Lstprue;
     private DPrueba1 dpre;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +20,22 @@ public class MnPrueba extends AppCompatActivity {
         setContentView(R.layout.activity_mn_prueba);
 
         Lstprue = (ListView) findViewById(R.id.FrmPrue_Lst);
+        schbus =  (SearchView) findViewById(R.id.FrmPrue_txtbus);
+
         dpre = new DPrueba1(this);
         dpre.Lst = Lstprue;
-        dpre.getListDatos("100");
+
+
+        schbus.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                dpre.getListDatos(s);
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
     }
 }
