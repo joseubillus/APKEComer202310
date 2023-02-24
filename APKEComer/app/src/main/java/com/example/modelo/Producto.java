95bab4f29f110c9b92a328e438dd2c1b44367c9b
joseubillus <jose_ubillus@hotmail.com>
2023-02-23 20:07:50 -0500

    prueba1

--- APKEComer/app/src/main/java/com/example/modelo/Producto.java
+++ APKEComer/app/src/main/java/com/example/modelo/Producto.java
@@ -1,4 +1,15 @@
 package com.example.modelo;
 
 public class Producto {
+    private int cod;
+    private String nom;
+    private double pre;
+    private int stock;
+    private String mar;
+    private String cat;
+    private String rank;
+    private String carac;
+    private String img;
+
+    
 }
